77731ea0b497956455066aaf77beba7d789b3fd6
root <kalinicham@ukr.net>
2019-05-06 17:36:59 +0300

    Step 3 - додав атрибут "is_surprise"

---
+++ Surprise/Setup/InstallData.php
@@ -0,0 +1,51 @@
+<?php
+namespace TSG\Surprise\Setup;
+
+use Magento\Eav\Model\Entity\Attribute\Source\Boolean;
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+use Magento\Catalog\Model\Product;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+class InstallData implements InstallDataInterface
+{
+    private $eavSetupFactory;
+
+    public function __construct(EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+        $eavSetup->addAttribute(
+            Product::ENTITY,
+            'is_surprise',
+            [
+                'group' => 'General',
+                'type' => 'int',
+                'backend' => '',
+                'frontend' => '',
+                'label' => 'is Surprise',
+                'input' => 'boolean',
+                'class' => '',
+                'source' => Boolean::class,
+                'global' => ScopedAttributeInterface::SCOPE_GLOBAL,
+                'visible' => true,
+                'required' => false,
+                'user_defined' => true,
+                'default' => 0,
+                'searchable' => false,
+                'filterable' => false,
+                'comparable' => false,
+                'visible_on_front' => false,
+                'used_in_product_listing' => false,
+                'unique' => false,
+                'apply_to' => ''
+            ]
+        );
+    }
+}
