0c8de4bfa9379bd503730285bbcbb3d25ebe9fc3
root <kalinicham@ukr.net>
2019-05-21 16:35:44 +0300

    Add Surprise 1

--- Surprise/Plugin/AddSurprise.php
+++ Surprise/Plugin/AddSurprise.php
@@ -4,21 +4,37 @@
 namespace TSG\Surprise\Plugin;
 
 use Magento\Framework\App\Request\Http;
+use Magento\Checkout\Model\Cart;
+use Magento\Catalog\Api\ProductRepositoryInterface;
 
 class AddSurprise
 {
     private $request;
 
+    protected $productRepository;
+
     public function __construct(
-        Http $request
+        Http $request,
+        Cart $cart,
+        ProductRepositoryInterface $productRepository
+
     ) {
         $this->request = $request;
+        $this->cart = $cart;
+        $this->productRepository = $productRepository;
     }
 
-    public function beforeExecute()
+    public function afterExecute()
     {
-        $params = $this->getRequest()->getParams();
-        $a =1;
+        $idsSurprise = $this->request->getParams()['surprise_product'];
+         if (!empty($idsSurprise)) {
+            // $surpriseProduct = $this->productRepository->getById($idsSurprise);
+             $params = [
+                 'price' => 0,
+                 'is_surprise' => 1,
+             ];
+             $this->cart->addProduct($idsSurprise, $params);
+             $this->cart->save();
+         }
     }
-
 }
