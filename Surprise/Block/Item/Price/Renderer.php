124eefd55ed146da5931755e7c43660c05a03ef9
root <kalinicham@ukr.net>
2019-05-24 19:29:06 +0300

    Check out

---
+++ Surprise/Block/Item/Price/Renderer.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 24.05.19
+ * Time: 15:42
+ */
+
+namespace TSG\Surprise\Block\Item\Price;
+
+
+use Magento\Framework\View\Element\Template;
+use Magento\Quote\Model\Quote\Item\AbstractItem as QuoteItem;
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\Serialize\Serializer\Json;
+
+class Renderer extends Template
+{
+
+    /**
+     * @var QuoteItem
+     */
+    protected $item;
+
+    /**
+     * @var string|int|null
+     */
+    protected $storeId = null;
+
+
+    protected $serializer;
+
+    public function __construct(
+        Template\Context $context,
+        array $data = [],
+        \Magento\Framework\Serialize\Serializer\Json $serializer = null)
+    {
+        $this->serializer = $serializer ?: ObjectManager::getInstance()->get(Json::class);
+        parent::__construct($context, $data);
+    }
+
+
+    /**
+     * Set item for render
+     *
+     * @param QuoteItem
+     * @return $this
+     */
+
+
+
+    public function setItem($item)
+    {
+        $this->item = $item;
+        $this->storeId = $item->getStoreId();
+        return $this;
+    }
+
+    public function getItem()
+    {
+           foreach ($this->item->getOptions() as $option){
+               if ($option->getCode() == 'info_buyRequest') {
+                 $param = $this->serializer->unserialize($option->getValue());
+                 if (array_key_exists('is_surprise', $param)) {
+                     return 'is_surprise';
+                 }
+               }
+            }
+        return $this->item;
+    }
+
+}
--- Surprise/Plugin/AddSurprise.php
+++ Surprise/Plugin/AddSurprise.php
@@ -4,37 +4,72 @@
 namespace TSG\Surprise\Plugin;
 
 use Magento\Framework\App\Request\Http;
-use Magento\Checkout\Model\Cart;
 use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Framework\DataObject\Factory as ObjectFactory;
 
 class AddSurprise
 {
+    /**
+     * @var Http
+     */
+
     private $request;
+
+    /**
+     * @var ProductRepositoryInterface
+     */
 
     protected $productRepository;
 
+    /**
+     * @var ObjectFactory
+     */
+
+    protected $objectFactory;
+
+    /**
+     * AddSurprise constructor.
+     * @param Http $request
+     * @param ProductRepositoryInterface $productRepository
+     * @param ObjectFactory $objectFactory
+     */
+
     public function __construct(
         Http $request,
-        Cart $cart,
-        ProductRepositoryInterface $productRepository
-
+        ProductRepositoryInterface $productRepository,
+        ObjectFactory $objectFactory
     ) {
         $this->request = $request;
-        $this->cart = $cart;
         $this->productRepository = $productRepository;
+        $this->objectFactory = $objectFactory;
     }
 
-    public function afterExecute()
+    /**
+     * Add surprise product to shopping cart (quote)
+     *
+     * @param $result \Magento\Checkout\Model\Cart
+     * @throws \Magento\Framework\Exception\LocalizedException
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
+
+    public function afterAddProduct($result)
     {
-        $idsSurprise = $this->request->getParams()['surprise_product'];
-         if (!empty($idsSurprise)) {
-            // $surpriseProduct = $this->productRepository->getById($idsSurprise);
-             $params = [
+        $params = $this->request->getParams();
+        if (array_key_exists("surprise_product", $params)) {
+             $surpriseProduct = $this->productRepository->getById($params['surprise_product']);
+             $surpriseProduct->setPrice(0);
+             $request = $this->objectFactory->create([
+                 'qty' => 1,
                  'price' => 0,
                  'is_surprise' => 1,
-             ];
-             $this->cart->addProduct($idsSurprise, $params);
-             $this->cart->save();
+             ]);
+            try {
+                $result->getQuote()->addProduct($surpriseProduct,$request);
+
+                } catch (\Exception $e)
+            {
+                    throw new \Magento\Framework\Exception\LocalizedException(__('The product does not exist.'));
+            }
          }
     }
 }
