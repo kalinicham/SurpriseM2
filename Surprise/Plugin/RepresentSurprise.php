5a1d337d3b207e21c24724a86fdb83500e637f44
kalinich <kalinicham@rambler.ru>
2019-06-03 08:30:32 -0400

    Home3

--- Surprise/Plugin/RepresentSurprise.php
+++ Surprise/Plugin/RepresentSurprise.php
@@ -2,13 +2,33 @@
 
 namespace TSG\Surprise\Plugin;
 
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\Serialize\Serializer\Json;
+
 class RepresentSurprise
 {
+
+    protected $serializer;
+
+    public function __construct(
+        Json $serializer = null
+    )
+    {
+        $this->serializer = $serializer ?: ObjectManager::getInstance()->get(Json::class);
+    }
+
+
     public function afterRepresentProduct($product, $result): bool
     {
         if ($result) {
-            if ($product->getCustomPrice() !== null) {
-               return false;
+            foreach ($product->getOptionsByCode() as $key => $option)
+            {
+                if ($key = "info_buyRequest") {
+                  $param = $this->serializer->unserialize($option->getValue());
+                   if (array_key_exists('is_surprise',$param) && $param['is_surprise']) {
+                     return false;
+                   }
+                }
             }
         }
         return $result;
