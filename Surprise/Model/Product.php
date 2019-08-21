c159b15ac1dd19c21bd8460dc78329af9a80c4e4
root <kalinicham@ukr.net>
2019-08-21 15:01:42 +0300

    MFTF Api

--- Surprise/Model/Product.php
+++ Surprise/Model/Product.php
@@ -7,98 +7,11 @@ use Magento\Catalog\Api\Data\ProductInterface;
 use Magento\Catalog\Model\FilterProductCustomAttribute;
 use Magento\Catalog\Model\Product\Attribute\Backend\Media\EntryConverterPool;
 use Magento\Framework\Api\AttributeValueFactory;
+use Magento\Framework\App\ObjectManager;
 use TSG\Surprise\Model\Product\Link;
 
 class Product extends \Magento\Catalog\Model\Product implements ProductInterface
 {
-    /**
-     * @var Link
-     */
-    protected $productLink;
-
-    /**
-     * @param \Magento\Framework\Model\Context $context
-     * @param \Magento\Framework\Registry $registry
-     * @param \Magento\Framework\Api\ExtensionAttributesFactory $extensionFactory
-     * @param AttributeValueFactory $customAttributeFactory
-     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
-     * @param \Magento\Catalog\Api\ProductAttributeRepositoryInterface $metadataService
-     * @param \Magento\Catalog\Model\Product\Url $url
-     * @param Link $productLink
-     * @param \Magento\Catalog\Model\Product\Configuration\Item\OptionFactory $itemOptionFactory
-     * @param \Magento\CatalogInventory\Api\Data\StockItemInterfaceFactory $stockItemFactory
-     * @param \Magento\Catalog\Model\Product\OptionFactory $catalogProductOptionFactory
-     * @param \Magento\Catalog\Model\Product\Visibility $catalogProductVisibility
-     * @param \Magento\Catalog\Model\Product\Attribute\Source\Status $catalogProductStatus
-     * @param \Magento\Catalog\Model\Product\Media\Config $catalogProductMediaConfig
-     * @param \Magento\Catalog\Model\Product\Type $catalogProductType
-     * @param \Magento\Framework\Module\Manager $moduleManager
-     * @param \Magento\Catalog\Helper\Product $catalogProduct
-     * @param \Magento\Catalog\Model\ResourceModel\Product $resource
-     * @param \Magento\Catalog\Model\ResourceModel\Product\Collection $resourceCollection
-     * @param \Magento\Framework\Data\CollectionFactory $collectionFactory
-     * @param \Magento\Framework\Filesystem $filesystem
-     * @param \Magento\Framework\Indexer\IndexerRegistry $indexerRegistry
-     * @param \Magento\Catalog\Model\Indexer\Product\Flat\Processor $productFlatIndexerProcessor
-     * @param \Magento\Catalog\Model\Indexer\Product\Price\Processor $productPriceIndexerProcessor
-     * @param \Magento\Catalog\Model\Indexer\Product\Eav\Processor $productEavIndexerProcessor
-     * @param CategoryRepositoryInterface $categoryRepository
-     * @param \Magento\Catalog\Model\Product\Image\CacheFactory $imageCacheFactory
-     * @param \Magento\Catalog\Model\ProductLink\CollectionProvider $entityCollectionProvider
-     * @param \Magento\Catalog\Model\Product\LinkTypeProvider $linkTypeProvider
-     * @param \Magento\Catalog\Api\Data\ProductLinkInterfaceFactory $productLinkFactory
-     * @param \Magento\Catalog\Api\Data\ProductLinkExtensionFactory $productLinkExtensionFactory
-     * @param EntryConverterPool $mediaGalleryEntryConverterPool
-     * @param \Magento\Framework\Api\DataObjectHelper $dataObjectHelper
-     * @param \Magento\Framework\Api\ExtensionAttribute\JoinProcessorInterface $joinProcessor
-     * @param array $data
-     * @param \Magento\Eav\Model\Config|null $config
-     * @param FilterProductCustomAttribute|null $filterCustomAttribute
-     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-     */
-    public function __construct(
-        \Magento\Framework\Model\Context $context,
-        \Magento\Framework\Registry $registry,
-        \Magento\Framework\Api\ExtensionAttributesFactory $extensionFactory,
-        AttributeValueFactory $customAttributeFactory,
-        \Magento\Store\Model\StoreManagerInterface $storeManager,
-        \Magento\Catalog\Api\ProductAttributeRepositoryInterface $metadataService,
-        \Magento\Catalog\Model\Product\Url $url,
-        Link $productLink,
-        \Magento\Catalog\Model\Product\Configuration\Item\OptionFactory $itemOptionFactory,
-        \Magento\CatalogInventory\Api\Data\StockItemInterfaceFactory $stockItemFactory,
-        \Magento\Catalog\Model\Product\OptionFactory $catalogProductOptionFactory,
-        \Magento\Catalog\Model\Product\Visibility $catalogProductVisibility,
-        \Magento\Catalog\Model\Product\Attribute\Source\Status $catalogProductStatus,
-        \Magento\Catalog\Model\Product\Media\Config $catalogProductMediaConfig,
-        \Magento\Catalog\Model\Product\Type $catalogProductType,
-        \Magento\Framework\Module\Manager $moduleManager,
-        \Magento\Catalog\Helper\Product $catalogProduct,
-        \Magento\Catalog\Model\ResourceModel\Product $resource,
-        \Magento\Catalog\Model\ResourceModel\Product\Collection $resourceCollection,
-        \Magento\Framework\Data\CollectionFactory $collectionFactory,
-        \Magento\Framework\Filesystem $filesystem,
-        \Magento\Framework\Indexer\IndexerRegistry $indexerRegistry,
-        \Magento\Catalog\Model\Indexer\Product\Flat\Processor $productFlatIndexerProcessor,
-        \Magento\Catalog\Model\Indexer\Product\Price\Processor $productPriceIndexerProcessor,
-        \Magento\Catalog\Model\Indexer\Product\Eav\Processor $productEavIndexerProcessor,
-        CategoryRepositoryInterface $categoryRepository,
-        \Magento\Catalog\Model\Product\Image\CacheFactory $imageCacheFactory,
-        \Magento\Catalog\Model\ProductLink\CollectionProvider $entityCollectionProvider,
-        \Magento\Catalog\Model\Product\LinkTypeProvider $linkTypeProvider,
-        \Magento\Catalog\Api\Data\ProductLinkInterfaceFactory $productLinkFactory,
-        \Magento\Catalog\Api\Data\ProductLinkExtensionFactory $productLinkExtensionFactory,
-        EntryConverterPool $mediaGalleryEntryConverterPool,
-        \Magento\Framework\Api\DataObjectHelper $dataObjectHelper,
-        \Magento\Framework\Api\ExtensionAttribute\JoinProcessorInterface $joinProcessor,
-        array $data = [],
-        \Magento\Eav\Model\Config $config = null,
-        FilterProductCustomAttribute $filterCustomAttribute = null
-    ) {
-        \Magento\Catalog\Model\Product::__construct($context, $registry, $extensionFactory, $customAttributeFactory, $storeManager, $metadataService, $url, $productLink, $itemOptionFactory, $stockItemFactory, $catalogProductOptionFactory, $catalogProductVisibility, $catalogProductStatus, $catalogProductMediaConfig, $catalogProductType, $moduleManager, $catalogProduct, $resource, $resourceCollection, $collectionFactory, $filesystem, $indexerRegistry, $productFlatIndexerProcessor, $productPriceIndexerProcessor, $productEavIndexerProcessor, $categoryRepository, $imageCacheFactory, $entityCollectionProvider, $linkTypeProvider, $productLinkFactory, $productLinkExtensionFactory, $mediaGalleryEntryConverterPool, $dataObjectHelper, $joinProcessor, $data, $config, $filterCustomAttribute);
-    }
-
     /**
      * Retrieve array of related products
      *
@@ -144,5 +57,10 @@ class Product extends \Magento\Catalog\Model\Product implements ProductInterface
         $collection = $this->getLinkInstance()->useSurpriseLinks()->getProductCollection()->setIsStrongMode();
         $collection->setProduct($this);
         return $collection;
+    }
+
+    public function getLinkInstance()
+    {
+        return ObjectManager::getInstance()->create(Link::class);
     }
 }
--- Surprise/Plugin/Related.php
+++ Surprise/Plugin/Related.php
@@ -4,12 +4,11 @@ namespace TSG\Surprise\Plugin;
 
 class Related
 {
-
-    public function beforeGetLinkedProducts($provider, $product) {
+    public function beforeGetLinkedProducts($provider, $product)
+    {
         $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-        $this->product = $objectManager->create('TSG\Surprise\Model\Product');
+        $this->product = $objectManager->create(\TSG\Surprise\Model\Product::class);
         $currentProduct = $this->product->load($product->getId());
         return [$currentProduct];
     }
-
 }
